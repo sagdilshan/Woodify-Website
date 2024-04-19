c6154cf33b9ea46a89509296fc52edd4a03ffa65
Gayathra Dilshan <gayathradilshan1@gmail.com>
2024-04-19 13:11:38 +0530

    cutomer

--- resources/views/website-pages/customer/customer_dashboard.blade.php
+++ resources/views/website-pages/customer/customer_dashboard.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>@yield('pageTitle') - Woodify</title>
-  <link rel="icon" href="../assets/dist/img/woodify-fav.png" />
+  <link rel="icon" href=" {{ asset('../assets/dist/img/woodify-fav.png') }}"/>
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
@@ -13,18 +13,15 @@
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{ asset('../assets/dist/css/adminlte.min.css') }}">
-
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css">
+
+
+    <link rel="stylesheet" href="{{ asset('../assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet"
+        href="{{ asset('../assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('../assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
 </head>
-<!--
-`body` tag options:
 
-  Apply one or more of the following classes to to the body tag
-  to get the desired effect
-
-  * sidebar-collapse
-  * sidebar-mini
--->
 <body class="hold-transition sidebar-mini sidebar-collapse layout-fixed ">
   <div class="wrapper">
     <!-- Navbar -->
@@ -52,43 +49,111 @@
 
   <!-- REQUIRED SCRIPTS -->
 
-  <!-- jQuery -->
-  <script src="{{ asset('../assets/plugins/jquery/jquery.min.js') }}"></script>
-  <!-- Bootstrap -->
-  <script src="{{ asset('../assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-  <!-- Woodify -->
-  <script src="{{ asset('../assets/dist/js/adminlte.js') }}"></script>
+    <!-- jQuery -->
+    <script src="{{ asset('../assets/plugins/jquery/jquery.min.js') }}"></script>
+    <!-- Bootstrap -->
+    <script src="{{ asset('../assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- Woodify -->
+    <script src="{{ asset('../assets/dist/js/adminlte.js') }}"></script>
 
-  <!-- OPTIONAL SCRIPTS -->
-  <script src="{{ asset('../assets/plugins/chart.js/Chart.min.js') }}"></script>
+    <!-- OPTIONAL SCRIPTS -->
+    <script src="{{ asset('../assets/plugins/chart.js/Chart.min.js') }}"></script>
 
-  <script src="{{ asset('../assets/dist/js/demo.js') }}"></script>
+    <script src="{{ asset('../assets/dist/js/demo.js') }}"></script>
 
-  <script src="{{ asset('../assets/dist/js/pages/dashboard3.js') }}"></script>
+    <script src="{{ asset('../assets/dist/js/pages/dashboard3.js') }}"></script>
 
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script src="{{ asset('../assets/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+      <script src="{{ asset('../assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+      <script src="{{ asset('../assets/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+      <script src="{{ asset('../assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+      <script src="{{ asset('../assets/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+      <script src="{{ asset('../assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+      <script src="{{ asset('../assets/plugins/jszip/jszip.min.js') }}"></script>
+      <script src="{{ asset('../assets/plugins/pdfmake/pdfmake.min.js') }}"></script>
+      <script src="{{ asset('../assets/plugins/pdfmake/vfs_fonts.js') }}"></script>
+      <script src="{{ asset('../assets/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+      <script src="{{ asset('../assets/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+      <script src="{{ asset('../assets/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+      <script>
+          @if (Session::has('message'))
+              var type = "{{ Session::get('alert-type', 'info') }}"
+              switch (type) {
+                  case 'info':
+                      toastr.info(" {{ Session::get('message') }} ");
+                      break;
 
-    <script>
-        @if (Session::has('message'))
-            var type = "{{ Session::get('alert-type', 'info') }}"
-            switch (type) {
-                case 'info':
-                    toastr.info(" {{ Session::get('message') }} ");
-                    break;
+                  case 'success':
+                      toastr.success(" {{ Session::get('message') }} ");
+                      break;
 
-                case 'success':
-                    toastr.success(" {{ Session::get('message') }} ");
-                    break;
+                  case 'warning':
+                      toastr.warning(" {{ Session::get('message') }} ");
+                      break;
 
-                case 'warning':
-                    toastr.warning(" {{ Session::get('message') }} ");
-                    break;
+                  case 'error':
+                      toastr.error(" {{ Session::get('message') }} ");
+                      break;
+              }
+          @endif
+      </script>
 
-                case 'error':
-                    toastr.error(" {{ Session::get('message') }} ");
-                    break;
-            }
-        @endif
+  <script>
+      $(function() {
+          $(document).on('click', '#delete', function(e) {
+              e.preventDefault();
+              var link = $(this).attr("href");
+
+
+              Swal.fire({
+                  title: 'Are you sure?',
+                  text: "You won't be able to revert this!",
+                  icon: 'warning',
+                  showCancelButton: true,
+                  confirmButtonColor: '#3085d6',
+                  cancelButtonColor: '#d33',
+                  confirmButtonText: 'Confirm'
+              }).then((result) => {
+                  if (result.isConfirmed) {
+                      window.location.href = link
+                      Swal.fire(
+                          'Deleted!',
+                          'Your data has been deleted.',
+                          'success'
+                      )
+                  }
+              })
+
+
+          });
+
+      });
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+  <script>
+      $(function() {
+          $("#example1").DataTable({
+              "responsive": true,
+              "lengthChange": false,
+              "autoWidth": false,
+              "buttons": ["excel", "pdf", "print"]
+          }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+
+      });
+  </script>
+
+  <script>
+      $(document).ready(function() {
+        $('.product-image-thumb').on('click', function () {
+          var $image_element = $(this).find('img')
+          $('.product-image').prop('src', $image_element.attr('src'))
+          $('.product-image-thumb.active').removeClass('active')
+          $(this).addClass('active')
+        })
+      })
     </script>
 </body>
 </html>
