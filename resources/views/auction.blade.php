27a1eef6ab8da9cdbcdf00da86b012bf4a9e1c04
Gayathra Dilshan <gayathradilshan1@gmail.com>
2024-05-10 20:32:00 +0530

    Update auction.blade.php

--- resources/views/auction.blade.php
+++ resources/views/auction.blade.php
@@ -34,42 +34,44 @@
 
                                 <div class="grid-products grid--view-items">
                                     <div class="row">
-                                        {{-- @foreach ($productss as $product)
+                                        @foreach ($auctions as $auction)
                                             <div class="col-md-6 col-sm-6 col-md-4 col-lg-3 grid-view-item style2 item">
                                                 <div class="grid-view_image">
                                                     <!-- start product image -->
-                                                    <a href="{{ route('product.view', ['id' => $product->id]) }}" class="grid-view-item__link">
+                                                    <a href="{{ route('auction.view', ['id' => $auction->id]) }}"
+                                                        class="grid-view-item__link">
                                                         <!-- image -->
                                                         <img class="grid-view-item__image primary blur-up lazyload"
-                                                            data-src="{{ !empty($product->thumb1) ? url('upload/thumb_images/' . $product->thumb1) : url('upload/no_product.png') }}"
-                                                            src="{{ !empty($product->thumb1) ? url('upload/thumb_images/' . $product->thumb1) : url('upload/no_product.png') }}"
-                                                            alt="{{ $product->name }}" title="{{ $product->name }}">
+                                                            data-src="{{ !empty($auction->thumb_1) ? url('upload/thumb_images/' . $auction->thumb_1) : url('upload/no_product.png') }}"
+                                                            src="{{ !empty($auction->thumb_1) ? url('upload/thumb_images/' . $auction->thumb_1) : url('upload/no_product.png') }}"
+                                                            alt="{{ $auction->p_name }}" title="{{ $auction->p_name }}">
                                                         <!-- End image -->
                                                         <!-- Hover image -->
                                                         <img class="grid-view-item__image hover blur-up lazyload"
-                                                            data-src="{{ !empty($product->thumb2) ? url('upload/thumb_images/' . $product->thumb2) : url('upload/no_product.png') }}"
-                                                            src="{{ !empty($product->thumb2) ? url('upload/thumb_images/' . $product->thumb2) : url('upload/no_product.png') }}"
-                                                            alt="{{ $product->name }}" title="{{ $product->name }}">
+                                                            data-src="{{ !empty($auction->thumb_2) ? url('upload/thumb_images/' . $auction->thumb_2) : url('upload/no_product.png') }}"
+                                                            src="{{ !empty($auction->thumb_2) ? url('upload/thumb_images/' . $auction->thumb_2) : url('upload/no_product.png') }}"
+                                                            alt="{{ $auction->p_name }}" title="{{ $auction->p_name }}">
                                                         <!-- End hover image -->
-                                                        <!-- product label -->
-                                                        <div class="product-labels rectangular"><span
-                                                                class="lbl on-sale">{{ $product->percentage_decrease }}%</span> <span
-                                                                class="lbl pr-label1">new</span></div>
-                                                        <!-- End product label -->
+
                                                     </a>
                                                     <!-- end product image -->
+                                                    <!-- countdown start -->
+                                                    <div class="saleTime desktop" data-countdown="{{ $auction->end_date }}">
+                                                    </div>
+                                                    <!-- countdown end -->
 
                                                     <!--start product details -->
                                                     <div class="product-details hoverDetails text-center mobile">
                                                         <!-- product name -->
                                                         <div class="product-name">
-                                                            <a href="{{ route('product.view', ['id' => $product->id]) }}">{{ $product->name }}</a>
+                                                            <a
+                                                                href="{{ route('auction.view', ['id' => $auction->id]) }}">{{ $auction->p_name }}</a>
                                                         </div>
                                                         <!-- End product name -->
                                                         <!-- product price -->
                                                         <div class="product-price">
-                                                            <span class="old-price">$ {{ $product->price }}</span>
-                                                            <span class="price">$ {{ $product->sale_price }}</span>
+                                                            {{-- <span class="old-price">$ {{ $auction->start_price }}</span> --}}
+                                                            <span class="price">$ {{ $auction->start_price }}</span>
                                                         </div>
                                                         <!-- End product price -->
 
@@ -79,7 +81,7 @@
                                                             <!-- Start product button -->
                                                             <form class="variants add">
                                                                 <a class="btn cartIcon btn-addto-cart"
-                                                                    href="{{ route('product.view', ['id' => $product->id]) }}"><i
+                                                                    href="{{ route('auction.view', ['id' => $auction->id]) }}"><i
                                                                         class="icon anm anm-eye-r"></i></a>
                                                             </form>
 
@@ -90,7 +92,7 @@
                                                 </div>
                                             </div>
                                         @endforeach
- --}}
+
 
 
                                     </div>
