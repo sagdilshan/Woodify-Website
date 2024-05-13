e9b3a90ff8918f9e989c64475f2f5ecebadc4381
Gayathra Dilshan <gayathradilshan1@gmail.com>
2024-05-13 18:04:32 +0530

    Update admin_dashboard.blade.php

--- resources/views/website-pages/admin/admin_dashboard.blade.php
+++ resources/views/website-pages/admin/admin_dashboard.blade.php
@@ -119,7 +119,17 @@
         });
     </script>
 
-    
+<script>
+    $(function() {
+        $("#example2").DataTable({
+            "responsive": true,
+            "lengthChange": false,
+            "autoWidth": false,
+            "buttons": ["excel", "pdf", "print"]
+        }).buttons().container().appendTo('#example2_wrapper .col-md-6:eq(0)');
+
+    });
+</script>
 
 
     <script>
