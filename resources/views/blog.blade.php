9d4135b28f9850ea0c48af8c95b78d6d91c8c3b3
Gayathra Dilshan <gayathradilshan1@gmail.com>
2024-06-04 09:35:32 +0530

    Update blog.blade.php

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -9,7 +9,7 @@
         <link rel="stylesheet" href="../../assetss/css/plugins.css">
         <!-- Boo tstap CSS -->
         <link rel="stylesheet" href="../../assetss/css/bootstrap.min.css">
-        <!-- Main Style CSS -->
+        <!-- Main Style CS -->
         <link rel="stylesheet" href="../../assetss/css/style.css">
         <link rel="stylesheet" href="../../assetss/css/responsive.css">
     </head>
